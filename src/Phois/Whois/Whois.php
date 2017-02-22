b3041755a928b8b0607abbca51d9f7cb944bf18a
Ilya S <shepik@yandex.ru>
2017-02-22 10:26:59 +0000

    fix: connection timeout

--- src/Phois/Whois/Whois.php
+++ src/Phois/Whois/Whois.php
@@ -49,6 +49,7 @@ class Whois
                     curl_setopt($ch, CURLOPT_URL, $url);
                     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
                     curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+                    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
                     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
